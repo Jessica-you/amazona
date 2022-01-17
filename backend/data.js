27f629639276b7a71d0050afbc5dd14e9083f81d
‘YOYO’ <952438659@qq.com>
2022-01-17 09:09:28 +0800

    更新

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products: [
+    {
+      _id: 1,
+      name: 'Nike Slim Shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 2,
+      name: 'Adidas Fit Shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 100,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 3,
+      name: 'Locoste Free Shirt',
+      category: 'Shirt',
+      image: '/images/p3.jpg',
+      price: 111,
+      countInStock: 0,
+      brand: 'Locoste',
+      rating: 4.0,
+      numReviews: 20,
+      description: 'high quality product'
+    },
+    {
+      _id: 4,
+      name: 'Puma Slim Pant',
+      category: 'Pant',
+      image: '/images/p4.jpg',
+      price: 120,
+      countInStock: 15,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 34,
+      description: 'high quality product'
+    },
+    {
+      _id: 5,
+      name: 'Nike Slim Pant',
+      category: 'Pant',
+      image: '/images/p5.jpg',
+      price: 110,
+      countInStock: 5,
+      brand: 'Nike',
+      rating: 4.2,
+      numReviews: 55,
+      description: 'high quality product'
+    },
+    {
+      _id: 6,
+      name: 'Adidas Slim Pant',
+      category: 'Pant',
+      image: '/images/p6.jpg',
+      price: 90,
+      countInStock: 12,
+      brand: 'Adidas',
+      rating: 4.8,
+      numReviews: 20,
+      description: 'high quality product'
+    }
+  ]
+}
+
+export default data;
